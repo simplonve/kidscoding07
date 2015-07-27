006f9408596330ca39bea530d5816a5492c4a5b2
[sandrineluquet] <[didpierrot@gmail.com]>
2015-07-27 17:04:54 +0200

    ajout des routes, de bodyParser et de FileSystem

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var fs = require('fs');
 var path = require('path');
-var routes = require('./routes/');
-
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.accueil);
-app.get('/inscription', routes.inscription);
+
+app.get('/', function(req, res){
+	res.render('accueil');
+});
+
+app.get('/inscription', function(req, res){
+	res.render('inscription');
+});
+
+app.post('/inscription', function(req, res){
+	if (req.body) { console.log(req.body); }
+	res.render('accueil');
+});
+
+
+fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+});
+
 
 app.listen(3000);
