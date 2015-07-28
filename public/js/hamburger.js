d7a08aa4514ebabf04031d4f0a88f4215a3039cb
p-guerin <guerin.pierre26@gmail.com>
2015-07-29 00:56:49 +0200

    Affichage de la nav quand scroll avec Jquery

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,13 +1,15 @@
-$('.kc-openmenu').click(function() {
-  $('.kc-menu').animate({
-      opacity: 1,
-      top: "0%",
-    }, 600);
-});
+$(document).ready(function() {
+  $('.kc-openmenu').click(function() {
+    $('.kc-menu').animate({
+        opacity: 1,
+        top: "0%",
+      }, 600);
+  });
 
-$('.kc-closemenu').click(function() {
-  $('.kc-menu').animate({
-      opacity: 0,
-      top: "-100%",
-    }, 600);
+  $('.kc-closemenu').click(function() {
+    $('.kc-menu').animate({
+        opacity: 0,
+        top: "-100%",
+      }, 600);
+  });
 });
---
+++ public/js/topnav.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(window).scroll(function(){
+    var scrolltop = $(window).scrollTop();
+    var h = $(window).height();
+    var div = $('.kc-topnav');
+
+    if(scrolltop > h || ){
+      if(div.css('opacity') == 0){
+        div.animate({opacity: 1, top: "0px"}, 200);
+      }
+    }else{
+      if(div.css('opacity') > 0){
+        div.animate({opacity: 0, top: "-70px"}, 200);
+      }
+    }
+  });
+});
